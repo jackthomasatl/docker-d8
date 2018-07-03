76c61bb258870f41f40807ec3ab356745e3b7710
Jack Thomas <Jack.Thomas@elavon.com>
2018-07-03 13:25:02 -0400

    Fixed drush version

--- php/5.6/fpm/scripts/install_drush.sh
+++ php/5.6/fpm/scripts/install_drush.sh
@@ -1,4 +1,4 @@
-#!/bin/#!/usr/bin/env bash
+#!/bin/bash
 
 export COMPOSER_ALLOW_SUPERUSER=1
 
@@ -6,6 +6,6 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 ln -s /usr/local/bin/composer /usr/bin/composer
 
-composer global require drush/drush:7.*
+composer global require drush/drush:8.*
 
 ln -s ~/.composer/vendor/bin/drush /usr/local/bin/drush
