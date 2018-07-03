a339d7a9af45f55b1b941971b04dea236ee78cb2
Jack Thomas <Jack.Thomas@elavon.com>
2018-07-03 11:44:15 -0400

    Base php image

---
+++ php/5.6/fpm/scripts/install_drush.sh
@@ -0,0 +1,11 @@
+#!/bin/#!/usr/bin/env bash
+
+export COMPOSER_ALLOW_SUPERUSER=1
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+ln -s /usr/local/bin/composer /usr/bin/composer
+
+composer global require drush/drush:7.*
+
+ln -s ~/.composer/vendor/bin/drush /usr/local/bin/drush
