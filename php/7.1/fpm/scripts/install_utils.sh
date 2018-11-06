01955ea2dd16a3de029ec0634cc281800939671e
Jack Thomas <Jack.Thomas@elavon.com>
2018-11-06 13:23:37 -0500

    updated container

--- php/7.1/fpm/scripts/install_utils.sh
+++ php/7.1/fpm/scripts/install_utils.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export COMPOSER_ALLOW_SUPERUSER=1
+
 apt-get update \
   && apt-get install -q -y --no-install-recommends \
     vim \
@@ -8,3 +10,11 @@ apt-get update \
     pv \
     git \
     openssh-client
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+ln -s /usr/local/bin/composer /usr/bin/composer
+
+composer global require drush/drush:8.*
+
+ln -s ~/.composer/vendor/bin/drush /usr/local/bin/drush
