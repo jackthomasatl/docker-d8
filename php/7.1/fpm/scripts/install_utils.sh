632c2818ff917b9579ba94fcf9091e7a00284db8
Jack Thomas <Jack.Thomas@elavon.com>
2019-01-08 15:47:53 -0500

    d8 updates

--- php/7.1/fpm/scripts/install_utils.sh
+++ php/7.1/fpm/scripts/install_utils.sh
@@ -15,6 +15,6 @@ curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 ln -s /usr/local/bin/composer /usr/bin/composer
 
-composer global require drush/drush:8.*
+composer global require drush/drush:9.*
 
 ln -s ~/.composer/vendor/bin/drush /usr/local/bin/drush
