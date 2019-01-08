34a8e13c7574b03f325c9e7a1ee662657744d6bc
Jack Thomas <Jack.Thomas@elavon.com>
2019-01-08 15:42:09 -0500

    removed 5.6

--- php/5.6/fpm/scripts/transport_https.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'deb http://cloudfront.debian.net/debian/ stretch main' > /etc/apt/sources.list
-
-apt-get update &&
-  apt-get install -y --no-install-recommends apt-transport-https apt-utils
